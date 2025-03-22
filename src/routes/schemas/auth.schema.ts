be166515481f87cb49c4848fa13b2895833d408b
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2025-03-22 16:20:00 -0300

    added stricter validation (regex of Capital Case, number and symbol) to register route

--- src/routes/schemas/auth.schema.ts
+++ src/routes/schemas/auth.schema.ts
@@ -7,7 +7,15 @@ export const RegisterSchema: FastifySchema = {
     z.object({
       email: z.string().email(),
       name: z.string(),
-      password: z.string().min(10),
+      password: z
+        .string()
+        .min(10, 'A senha deve conter ao menos 10 caracteres')
+        .regex(/[A-Z]/, 'A senha deve conter ao menos uma letra maiúscula')
+        .regex(/[0-9]/, 'A senha deve conter ao menos um número')
+        .regex(
+          /[^A-Za-z0-9\s]/,
+          'A senha deve conter ao menos um caractere especial (não sendo espaço)'
+        ),
     })
   ),
 };
@@ -16,7 +24,7 @@ export const LoginSchema: FastifySchema = {
   body: zodToJsonSchema(
     z.object({
       email: z.string().email(),
-      password: z.string(),
+      password: z.string(), // don't validate password for login, as to not expose the requirements to the user.
     })
   ),
 };
